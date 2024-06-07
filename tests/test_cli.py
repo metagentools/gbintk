41cd8ca49a83df75f9b534df969292bb2a05a9b4
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2024-06-07 11:50:29 +0930

    TST: Update test cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import pytest
 
 from click.testing import CliRunner
 
-from gbintk.cli import graphbin, graphbin2, metacoag
+from gbintk.cli import graphbin, graphbin2, metacoag, visualise
 
 
 __author__ = "Vijini Mallawaarachchi"
@@ -58,4 +58,14 @@ def test_metacoag_run(runner, tmp_dir):
     abundance = DATADIR / "Sim-5G+metaSPAdes" / "coverm_mean_coverage.tsv"
     args = f"--assembler spades --graph {graph} --contigs {contigs} --paths {paths} --abundance {abundance} --output {outpath}".split()
     r = runner.invoke(metacoag, args, catch_exceptions=False)
+    assert r.exit_code == 0, r.output
+
+def test_visualise_run(runner, tmp_dir):
+    outpath = tmp_dir
+    initial = DATADIR / "Sim-5G+metaSPAdes" / "metacoag_res.csv"
+    final = DATADIR / "Sim-5G+metaSPAdes" / "graphbin_res.csv"
+    graph = DATADIR / "Sim-5G+metaSPAdes" / "assembly_graph_with_scaffolds.gfa"
+    paths = DATADIR / "Sim-5G+metaSPAdes" / "contigs.paths"
+    args = f"--assembler spades --initial {initial} --final {final} --graph {graph} --paths {paths} --output {outpath}".split()
+    r = runner.invoke(visualise, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
